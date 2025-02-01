7be9fab796388721a75272136028853f0411f2a7
Anan Hamdy <nonahamdy205@gmail.com>
2025-02-02 01:33:25 +0200

    implementing navigation in nav bar

--- lib/feautres/nav bar/presentation/views/main_home_view.dart
+++ lib/feautres/nav bar/presentation/views/main_home_view.dart
@@ -1,64 +1,68 @@
-
 import 'package:ecommerce_app/core/app_colors.dart';
+import 'package:ecommerce_app/feautres/nav%20bar/presentation/manager/cubit/nav_bar_cubit.dart';
 import 'package:ecommerce_app/feautres/nav%20bar/presentation/views/widgets/favorite_view.dart';
 import 'package:ecommerce_app/feautres/nav%20bar/presentation/views/widgets/home_view.dart';
-import 'package:ecommerce_app/feautres/nav%20bar/presentation/views/widgets/main_home_view_body.dart';
 import 'package:ecommerce_app/feautres/nav%20bar/presentation/views/widgets/profile_view.dart';
 import 'package:ecommerce_app/feautres/nav%20bar/presentation/views/widgets/store_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 
 class MainHomeView extends StatelessWidget {
   const MainHomeView({super.key});
-final List<Widget> views = const[
- HomeView(),
-  StoreView(),
-  FavoriteView(),
-  ProfileView(),
-
-];
+  final List<Widget> views = const [
+    HomeView(),
+    StoreView(),
+    FavoriteView(),
+    ProfileView(),
+  ];
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      body:const MainHomeViewBody(),
-      bottomNavigationBar: Container(
-        decoration:const BoxDecoration(
-          color: AppColors.kWhiteColor
-        ),
-        child: Padding(
-          padding:const EdgeInsets.symmetric(vertical: 5,horizontal: 5),
-          child: GNav(
-            onTabChange: (value){
-                       
-                        },
-            rippleColor: AppColors.kPrimaryColor,
-            hoverColor: AppColors.kPrimaryColor,
-            curve: Curves.easeOutExpo,
-            gap: 3,
-            activeColor: AppColors.kWhiteColor,
-            iconSize: 24,
-            tabBackgroundColor: AppColors.kPrimaryColor,
-            color: Colors.grey,
-            tabs:const [
-            GButton(
-              icon: Icons.home,
-              text: 'Home',
-            ),
-            GButton(
-              icon: Icons.store,
-              text: 'Store',
-            ),
-            GButton(
-              icon: Icons.favorite,
-              text: 'Favorite',
-            ),
-            GButton(
-              icon: Icons.person,
-              text: 'Profile',
-            ),
-          ],
-              ),
-        ),
-      ));
+    return BlocProvider(
+      create: (context) => NavBarCubit(),
+      child: BlocBuilder<NavBarCubit, NavBarState>(
+        builder: (context, state) {
+          NavBarCubit cubit = context.read<NavBarCubit>();
+          return Scaffold(
+              body: views[cubit.currentIndex],
+              bottomNavigationBar: Container(
+                decoration: const BoxDecoration(color: AppColors.kWhiteColor),
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 5, horizontal: 5),
+                  child: GNav(
+                    onTabChange: cubit.changeIndex,
+                    rippleColor: AppColors.kPrimaryColor,
+                    hoverColor: AppColors.kPrimaryColor,
+                    curve: Curves.easeOutExpo,
+                    gap: 3,
+                    activeColor: AppColors.kWhiteColor,
+                    iconSize: 24,
+                    tabBackgroundColor: AppColors.kPrimaryColor,
+                    color: Colors.grey,
+                    tabs: const [
+                      GButton(
+                        icon: Icons.home,
+                        text: 'Home',
+                      ),
+                      GButton(
+                        icon: Icons.store,
+                        text: 'Store',
+                      ),
+                      GButton(
+                        icon: Icons.favorite,
+                        text: 'Favorite',
+                      ),
+                      GButton(
+                        icon: Icons.person,
+                        text: 'Profile',
+                      ),
+                    ],
+                  ),
+                ),
+              ));
+        },
+      ),
+    );
   }
 }
