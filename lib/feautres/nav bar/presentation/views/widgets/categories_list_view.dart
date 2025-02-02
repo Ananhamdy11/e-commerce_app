848b23f272cf5f664b112a5da20fb081360d0cbe
Anan Hamdy <nonahamdy205@gmail.com>
2025-02-02 02:36:36 +0200

    refactor categories list

---
+++ lib/feautres/nav bar/presentation/views/widgets/categories_list_view.dart
@@ -0,0 +1,54 @@
+import 'package:ecommerce_app/core/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class CategoriesList extends StatelessWidget {
+  const CategoriesList({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+     height: 100,
+      child: ListView.builder(
+       scrollDirection: Axis.horizontal,
+        itemCount: categories.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                CircleAvatar(
+                 radius: 30,
+                
+                 backgroundColor: AppColors.kPrimaryColor,
+                  child: Icon(categories[index].icon,color: AppColors.kWhiteColor,size: 40,),
+                ),
+                Text(categories[index].title),
+              ],
+            ),
+            
+          )  ;
+        },
+      ),
+    );
+  }
+}
+
+List <Category> categories = [
+  Category(icon: Icons.sports, title: 'Sports'),
+  Category(icon: Icons.tv, title: 'Electronics'),
+  Category(icon: Icons.collections, title: 'Collections'),
+  Category(icon: Icons.book, title: 'Books'),
+  Category(icon: Icons.games, title: 'Games'),
+  Category(icon: Icons.bike_scooter, title: 'Bikes'),
+
+];
+class Category{
+
+  final IconData icon;
+  final String title;
+
+  Category({required this.icon, required this.title});
+
+}
--- lib/feautres/nav bar/presentation/views/widgets/home_view.dart
+++ lib/feautres/nav bar/presentation/views/widgets/home_view.dart
@@ -1,4 +1,4 @@
-import 'package:ecommerce_app/core/app_colors.dart';
+import 'package:ecommerce_app/feautres/nav%20bar/presentation/views/widgets/categories_list_view.dart';
 import 'package:ecommerce_app/feautres/nav%20bar/presentation/views/widgets/custom_search_field.dart';
 import 'package:flutter/material.dart';
 
@@ -19,30 +19,7 @@ class HomeView extends StatelessWidget {
             fontSize: 20,
           ),),
          const SizedBox(height: 20,),
-         SizedBox(
-          height: 100,
-           child: ListView.builder(
-            scrollDirection: Axis.horizontal,
-             itemCount: categories.length,
-             itemBuilder: (BuildContext context, int index) {
-               return Padding(
-                 padding: const EdgeInsets.all(8.0),
-                 child: Column(
-                   children: [
-                     CircleAvatar(
-                      radius: 30,
-                     
-                      backgroundColor: AppColors.kPrimaryColor,
-                       child: Icon(categories[index].icon,color: AppColors.kWhiteColor,size: 40,),
-                     ),
-                     Text(categories[index].title),
-                   ],
-                 ),
-                 
-               )  ;
-             },
-           ),
-         ),
+         const CategoriesList(),
 
 
         ],
@@ -50,19 +27,4 @@ class HomeView extends StatelessWidget {
     );
   }
 }
-List <Category> categories = [
-  Category(icon: Icons.sports, title: 'Sports'),
-  Category(icon: Icons.tv, title: 'Electronics'),
-  Category(icon: Icons.collections, title: 'Collections'),
-  Category(icon: Icons.book, title: 'Books'),
-  Category(icon: Icons.games, title: 'Games'),
-  Category(icon: Icons.bike_scooter, title: 'Bikes'),
 
-];
-class Category{
-  final IconData icon;
-  final String title;
-
-  Category({required this.icon, required this.title});
-
-}
