75a607ed10918d6d10989d9b649fb693e022ad69
Anan Hamdy <nonahamdy205@gmail.com>
2025-02-02 23:01:37 +0200

    adding cached network image

---
+++ lib/core/helper/widgets/cached_image.dart
@@ -0,0 +1,27 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:ecommerce_app/core/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class CachednetworkImage extends StatelessWidget {
+  const CachednetworkImage({
+    super.key, required this.url,
+  });
+final String url;
+  @override
+  Widget build(BuildContext context) {
+    return CachedNetworkImage(
+      imageUrl:url,
+      placeholder: (context, url) => const SizedBox(
+        height: 200.0,
+        child: Center(
+          child: CircularProgressIndicator(
+            color: AppColors.kPrimaryColor,
+          ),
+        ),
+        
+      ),
+      errorWidget: (context, url, error) =>const Icon(Icons.error),
+    );
+  }
+}
+
--- lib/feautres/nav bar/presentation/views/widgets/home_view.dart
+++ lib/feautres/nav bar/presentation/views/widgets/home_view.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables
 
 import 'package:ecommerce_app/core/app_colors.dart';
+import 'package:ecommerce_app/core/helper/widgets/cached_image.dart';
 import 'package:ecommerce_app/core/helper/widgets/custom_evb.dart';
 import 'package:ecommerce_app/feautres/nav%20bar/presentation/views/widgets/categories_list_view.dart';
 import 'package:ecommerce_app/feautres/nav%20bar/presentation/views/widgets/custom_search_field.dart';
@@ -44,16 +45,17 @@ class HomeView extends StatelessWidget {
               children: [
                 Stack(
                   children: [
-                    const ClipRRect(
-                      borderRadius: BorderRadius.only(
+                   const ClipRRect(
+                      borderRadius:  BorderRadius.only(
                         topRight: Radius.circular(16),
                         bottomLeft: Radius.circular(16),
                         bottomRight: Radius.circular(16),
                       ),
-                      child: Image(
-                        image: NetworkImage(
-                            'https://img.freepik.com/premium-psd/isolated-smart-tv-monitor-home-office-entertainment_92267-199.jpg?w=996'),
-                      ),
+                      child: CachednetworkImage(url:'https://img.freepik.com/premium-psd/isolated-smart-tv-monitor-home-office-entertainment_92267-199.jpg?w=996' ,),
+                      // Image(
+                      //   image: NetworkImage(
+                      //       'https://img.freepik.com/premium-psd/isolated-smart-tv-monitor-home-office-entertainment_92267-199.jpg?w=996'),
+                      // ),
                     ),
                     Positioned(
                       child: Container(
@@ -77,74 +79,64 @@ class HomeView extends StatelessWidget {
                   ],
                 ),
                 const SizedBox(
-            height: 15,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                   const Text(
-                      'product name ',
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold
+                  height: 15,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text(
+                            'product name ',
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          ),
+                          IconButton(
+                            onPressed: () {},
+                            icon: const Icon(Icons.favorite),
+                            color: AppColors.kGreyColor,
+                          )
+                        ],
                       ),
-                    ),
-                    IconButton(
-                      onPressed: () {},
-                      icon:const Icon(Icons.favorite),
-                      color: AppColors.kGreyColor,
-                    )
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    const Column(
-                       children: [
-                         Text(
-                          '1000 LE ',
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Column(
+                            children: [
+                              Text(
+                                '1000 LE ',
+                                style: TextStyle(
+                                    fontSize: 18, fontWeight: FontWeight.bold),
+                              ),
+                              Text(
+                                '1200 LE ',
+                                style: TextStyle(
+                                    decoration: TextDecoration.lineThrough,
+                                    fontWeight: FontWeight.bold,
+                                    color: AppColors.kGreyColor),
+                              ),
+                            ],
                           ),
-                                             ),
-                                             Text(
-                          '1200 LE ',
-                          style: TextStyle(
-                            decoration: TextDecoration.lineThrough,
-                            fontWeight: FontWeight.bold,
-                            color: AppColors.kGreyColor
-                          ),
-                                             ),
-
-                       ],
-                     ),
-                     CustomEVB(
-                      onTap: () {},
-                       widget: const Text(
-                        'Buy Now',
-                        style: TextStyle(
-                          color: AppColors.kWhiteColor
-                        ),
-                      ),)
-                  ],
+                          CustomEVB(
+                            onTap: () {},
+                            widget: const Text(
+                              'Buy Now',
+                              style: TextStyle(color: AppColors.kWhiteColor),
+                            ),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
-
               ],
             ),
           ),
-              ],
-            ),
-          ),
-          
-
-          
         ],
       ),
     );
   }
 }
+
