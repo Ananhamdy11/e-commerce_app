ee4d0b3a71b362d4bdf5e44a2422ae9b718083ae
Anan Hamdy <nonahamdy205@gmail.com>
2025-02-02 02:32:29 +0200

    add categories list in home view

--- lib/feautres/nav bar/presentation/views/widgets/home_view.dart
+++ lib/feautres/nav bar/presentation/views/widgets/home_view.dart
@@ -13,10 +13,56 @@ class HomeView extends StatelessWidget {
         children:[
          const CustomSearchField(),
           const SizedBox(height: 20,),
-           Image.asset('assets/images/buy.jpg'),
-           
+          Image.asset('assets/images/buy.jpg'),
+          const SizedBox(height: 20,),
+          const Text('Popular Categories',style: TextStyle(
+            fontSize: 20,
+          ),),
+         const SizedBox(height: 20,),
+         SizedBox(
+          height: 100,
+           child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+             itemCount: categories.length,
+             itemBuilder: (BuildContext context, int index) {
+               return Padding(
+                 padding: const EdgeInsets.all(8.0),
+                 child: Column(
+                   children: [
+                     CircleAvatar(
+                      radius: 30,
+                     
+                      backgroundColor: AppColors.kPrimaryColor,
+                       child: Icon(categories[index].icon,color: AppColors.kWhiteColor,size: 40,),
+                     ),
+                     Text(categories[index].title),
+                   ],
+                 ),
+                 
+               )  ;
+             },
+           ),
+         ),
+
+
         ],
       ),
     );
   }
+}
+List <Category> categories = [
+  Category(icon: Icons.sports, title: 'Sports'),
+  Category(icon: Icons.tv, title: 'Electronics'),
+  Category(icon: Icons.collections, title: 'Collections'),
+  Category(icon: Icons.book, title: 'Books'),
+  Category(icon: Icons.games, title: 'Games'),
+  Category(icon: Icons.bike_scooter, title: 'Bikes'),
+
+];
+class Category{
+  final IconData icon;
+  final String title;
+
+  Category({required this.icon, required this.title});
+
 }
