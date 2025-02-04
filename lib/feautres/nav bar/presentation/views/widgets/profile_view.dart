6886472dcf9c66781dfa94782bb68fa9ee27ef5d
Anan Hamdy <nonahamdy205@gmail.com>
2025-02-04 15:42:23 +0200

    create card in profile view ui

--- lib/feautres/nav bar/presentation/views/widgets/profile_view.dart
+++ lib/feautres/nav bar/presentation/views/widgets/profile_view.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce_app/core/app_colors.dart';
 import 'package:flutter/material.dart';
 
 class ProfileView extends StatelessWidget {
@@ -5,12 +6,61 @@ class ProfileView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  const Center(
-      child: Column(
-        children: [
-          Text('Profile View'),
-        ],
-      ),
-    );
+    return     const  Card(
+            margin:  EdgeInsets.all(20),
+            color: AppColors.kWhiteColor,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(16)
+              )
+            ),
+            child: Padding(
+              padding:  EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CircleAvatar(
+                    radius: 50,
+                    backgroundColor: AppColors.kPrimaryColor,
+                    foregroundColor: AppColors.kWhiteColor,
+                    child: Icon(Icons.person,size: 50,),
+                  ),
+                   Text('User Name',style: TextStyle(
+                    fontWeight: FontWeight.bold
+                  ),),
+                   SizedBox(height: 10,),
+                   Text('Your Email'),
+                   SizedBox(height: 10,),
+
+                  Card(
+                    color: AppColors.kPrimaryColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(16)
+                      )
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Icon(Icons.person,color: AppColors.kWhiteColor,),
+                          Text('Edit Name',style: TextStyle(
+                            color: AppColors.kWhiteColor,
+                            fontWeight: FontWeight.bold
+                          ),),
+                         Icon(Icons.arrow_forward_ios,color: AppColors.kWhiteColor,),
+                      
+                        ],
+                      ),
+                    ),
+                  )
+               
+                ],
+              ),
+            ),
+          );
+          
+    
   }
 }
