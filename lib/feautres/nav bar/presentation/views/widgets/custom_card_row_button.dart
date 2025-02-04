48239ce52b45caa6353161630cdbaa578803c1d3
Anan Hamdy <nonahamdy205@gmail.com>
2025-02-04 16:06:41 +0200

    refactor profile view ui

---
+++ lib/feautres/nav bar/presentation/views/widgets/custom_card_row_button.dart
@@ -0,0 +1,41 @@
+import 'package:ecommerce_app/core/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class CustomCardRowButton extends StatelessWidget {
+  const CustomCardRowButton({
+    super.key, this.onTap, required this.icon, required this.text,
+  });
+final void Function()? onTap;
+final IconData icon;
+final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Card(
+        color: AppColors.kPrimaryColor,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(12)
+          )
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Icon(icon,color: AppColors.kWhiteColor,),
+              Text(text,style: const TextStyle(
+                color: AppColors.kWhiteColor,
+                fontWeight: FontWeight.bold
+              ),),
+             const Icon(Icons.arrow_forward_ios,color: AppColors.kWhiteColor,),
+          
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/feautres/nav bar/presentation/views/widgets/profile_view.dart
+++ lib/feautres/nav bar/presentation/views/widgets/profile_view.dart
@@ -1,4 +1,5 @@
 import 'package:ecommerce_app/core/app_colors.dart';
+import 'package:ecommerce_app/feautres/nav%20bar/presentation/views/widgets/custom_card_row_button.dart';
 import 'package:flutter/material.dart';
 
 class ProfileView extends StatelessWidget {
@@ -65,41 +66,3 @@ class ProfileView extends StatelessWidget {
   }
 }
 
-class CustomCardRowButton extends StatelessWidget {
-  const CustomCardRowButton({
-    super.key, this.onTap, required this.icon, required this.text,
-  });
-final void Function()? onTap;
-final IconData icon;
-final String text;
-
-  @override
-  Widget build(BuildContext context) {
-    return InkWell(
-      onTap: onTap,
-      child: Card(
-        color: AppColors.kPrimaryColor,
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(
-            Radius.circular(12)
-          )
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(12.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Icon(icon,color: AppColors.kWhiteColor,),
-              Text(text,style: const TextStyle(
-                color: AppColors.kWhiteColor,
-                fontWeight: FontWeight.bold
-              ),),
-             const Icon(Icons.arrow_forward_ios,color: AppColors.kWhiteColor,),
-          
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
