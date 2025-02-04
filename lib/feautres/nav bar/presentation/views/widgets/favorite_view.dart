13f5d5e0233b5395e479516bdc05d85c6a9b8a27
Anan Hamdy <nonahamdy205@gmail.com>
2025-02-04 15:13:39 +0200

    finish favorite view ui

--- lib/feautres/nav bar/presentation/views/widgets/favorite_view.dart
+++ lib/feautres/nav bar/presentation/views/widgets/favorite_view.dart
@@ -1,3 +1,5 @@
+import 'package:ecommerce_app/core/helper/widgets/product_list.dart';
+import 'package:ecommerce_app/feautres/nav%20bar/presentation/views/widgets/custom_search_field.dart';
 import 'package:flutter/material.dart';
 
 class FavoriteView extends StatelessWidget {
@@ -5,12 +7,27 @@ class FavoriteView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const  Center(
-      child: Column(
-        children: [
-          Text('Favorite View'),
+     return Padding(
+      padding:const EdgeInsets.all(10.0),
+      child: ListView(
+        children:const [
+           Center(
+             child: Text('Your Favorite Products ',
+                       style:TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold
+                       
+                       ) ,
+                       ),
+           ),
+           SizedBox(height:20 ,),
+          CustomSearchField(),
+           SizedBox(height:20 ,),
+           ProductList()
+
         ],
       ),
-    );
+      );
+ 
   }
 }
