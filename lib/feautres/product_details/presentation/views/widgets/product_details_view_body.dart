425146f27040c4f9afc74f34cb657412c2f1e09f
Anan Hamdy <nonahamdy205@gmail.com>
2025-02-07 02:24:16 +0200

    add rating bar for product details view

--- lib/feautres/product_details/presentation/views/widgets/product_details_view_body.dart
+++ lib/feautres/product_details/presentation/views/widgets/product_details_view_body.dart
@@ -1,5 +1,7 @@
+import 'package:ecommerce_app/core/app_colors.dart';
 import 'package:ecommerce_app/core/helper/widgets/cached_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 class ProductDetailsViewBody extends StatelessWidget {
   const ProductDetailsViewBody({super.key});
@@ -7,10 +9,56 @@ class ProductDetailsViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView(
-      children: [
-        CachednetworkImage(
+      children:   [
+        const CachednetworkImage(
           url: 'https://img.freepik.com/premium-psd/isolated-smart-tv-monitor-home-office-entertainment_92267-199.jpg?w=996'
           ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10,vertical: 16),
+            child: Column(
+              children: [
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children:  [
+                    Text('books'),
+                    Text('200\$')
+                  ],
+                ),
+                 const SizedBox(height: 20,),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children:  [
+                    Row(
+                      children: [
+                        Text('3 '),
+                        Icon(Icons.star,color: Colors.amber,)
+                      ],
+                    ),
+                    Icon(Icons.favorite,color: AppColors.kGreyColor,)
+                  ],
+                ),
+                const SizedBox(height: 30,),
+                const Text('Product description'),
+                const SizedBox(height: 20,),
+                RatingBar.builder(
+   initialRating: 3,
+   minRating: 1,
+   direction: Axis.horizontal,
+   allowHalfRating: false,
+   itemCount: 5,
+   itemPadding: const EdgeInsets.symmetric(horizontal: 4.0),
+   itemBuilder: (context, _) => const Icon(
+     Icons.star,
+     color: Colors.amber,
+   ),
+   onRatingUpdate: (rating) {
+     print(rating);
+   },
+)
+
+              ],
+            ),
+          )
 
       ],
     );
